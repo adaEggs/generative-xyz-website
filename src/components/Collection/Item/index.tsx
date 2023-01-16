21b7b105875e43a5c015b7dc0953a565e83b1b84
trilm123456 <75830532+trilm123456@users.noreply.github.com>
2023-01-16 17:20:54 +0700

    Add responsive for collect and project detail page (#87)
    * style: responsive collect page
    
    * style: responsive for project detail
    
    Co-authored-by: Duc Anh Nguyen <ducanh@myconstant.com>

--- src/components/Collection/Item/index.tsx
+++ src/components/Collection/Item/index.tsx
@@ -8,12 +8,19 @@ import { LogLevel } from '@enums/log-level';
 import { Token } from '@interfaces/token';
 import { User } from '@interfaces/user';
 import { getListing } from '@services/marketplace';
-import { formatTokenId, getProjectIdFromTokenId } from '@utils/format';
+import {
+  formatAddress,
+  formatTokenId,
+  getProjectIdFromTokenId,
+} from '@utils/format';
 import log from '@utils/logger';
 import { useContext, useEffect, useMemo, useState } from 'react';
 import { Stack } from 'react-bootstrap';
 import Web3 from 'web3';
 import s from './styles.module.scss';
+import cs from 'classnames';
+import useWindowSize from '@hooks/useWindowSize';
+import Text from '@components/Text';
 
 const CollectionItem = ({
   data,
@@ -25,6 +32,7 @@ const CollectionItem = ({
   const tokenID = useMemo(() => data.name.split('#')[1], [data.name]);
   const [listingTokenPrice, setListingTokenPrice] = useState('0');
   const { currentUser } = useContext(ProfileContext);
+  const { isMobile } = useWindowSize();
 
   const handleFetchListingTokenPrice = async () => {
     try {
@@ -81,32 +89,54 @@ const CollectionItem = ({
             loading={'lazy'}
           />
         </div>
-        <div className={s.collectionCard_info}>
-          {data.owner ? (
-            <CreatorInfo creator={data.owner as User} />
-          ) : (
-            <CreatorInfo creator={{ walletAddress: data.ownerAddr } as User} />
-          )}
-          <div className={s.collectionCard_info_title}>
-            <Stack
-              className={s.collectionCard_info_stack}
-              direction="horizontal"
-            >
-              <Heading as={'h4'} className="token_id ml-auto">
-                {currentUser && `${data.project.name} `}#
-                {formatTokenId(tokenID)}
-              </Heading>
-              {listingTokenPrice !== '0' && (
-                <Stack
-                  direction="horizontal"
-                  className={s.collectionCard_listing}
-                >
-                  <b>Ξ{listingTokenPrice}</b>
-                </Stack>
-              )}
-            </Stack>
+        {isMobile ? (
+          <div className={cs(s.collectionCard_info, s.mobile)}>
+            <Text size="11" fontWeight="medium">
+              {data?.owner?.displayName ||
+                formatAddress(
+                  data?.owner?.walletAddress || data?.ownerAddr || ''
+                )}
+            </Text>
+            <div className={s.collectionCard_info_title}>
+              <Text size="14" fontWeight="semibold">
+                #{formatTokenId(tokenID)}
+              </Text>
+
+              <Text size="14" fontWeight="bold">
+                {listingTokenPrice !== '0' ? `Ξ${listingTokenPrice}` : ''}
+              </Text>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className={cs(s.collectionCard_info, s.desktop)}>
+            {data.owner ? (
+              <CreatorInfo creator={data.owner as User} />
+            ) : (
+              <CreatorInfo
+                creator={{ walletAddress: data.ownerAddr } as User}
+              />
+            )}
+            <div className={s.collectionCard_info_title}>
+              <Stack
+                className={s.collectionCard_info_stack}
+                direction="horizontal"
+              >
+                <Heading as={'h4'} className="token_id ml-auto">
+                  {currentUser && `${data.project.name} `}#
+                  {formatTokenId(tokenID)}
+                </Heading>
+                {listingTokenPrice !== '0' && (
+                  <Stack
+                    direction="horizontal"
+                    className={s.collectionCard_info_listing}
+                  >
+                    <b>Ξ{listingTokenPrice}</b>
+                  </Stack>
+                )}
+              </Stack>
+            </div>
+          </div>
+        )}
       </div>
     </Link>
   );
