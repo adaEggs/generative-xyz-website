a5c693589de65cbb77f437b98559b5eeb91ffaca
Dr. Cat <dr.cat911@generative.xyz>
2023-02-02 17:13:32 +0700

    fix: skeleton loading in the collection page

--- src/components/ProjectListLoading/index.tsx
+++ src/components/ProjectListLoading/index.tsx
@@ -1,24 +1,14 @@
 import ProjectCardSkeleton from '@components/ProjectCard/skeleton';
-import useWindowSize from '@hooks/useWindowSize';
 import { v4 } from 'uuid';
 
-const ProjectListLoading = ({
-  numOfItems = 8,
-}: // cols = 4,
-{
-  numOfItems?: number;
-  // cols?: number;
-}) => {
-  const { mobileScreen } = useWindowSize();
-
+const ProjectListLoading = ({ numOfItems = 12 }: { numOfItems?: number }) => {
   return (
-    <div
-      className={`grid gap-24 ${
-        mobileScreen ? 'grid-cols-2' : 'grid-auto-fit'
-      }`}
-    >
+    <div className={'row'}>
       {[...Array(numOfItems)].map(() => (
-        <ProjectCardSkeleton key={`token-loading-${v4()}`} />
+        <ProjectCardSkeleton
+          className={'col-wide-2_5 col-xl-3 col-lg-5 col-6'}
+          key={`token-loading-${v4()}`}
+        />
       ))}
     </div>
   );
