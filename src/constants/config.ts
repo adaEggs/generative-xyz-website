93fe2c171ab669d4e926798a5463008586b1e6e3
benjiro2077 <benjiro2077@generative.xyz>
2023-02-13 09:58:37 +0700

    refactor: remove redundant packages

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -10,24 +10,6 @@ export const NETWORK_CHAIN_ID: number = parseInt(
 );
 export const CDN_URL = process.env.NEXT_PUBLIC_CDN_URL!;
 export const APP_TOKEN_SYMBOL = 'GEN';
-
-// Datadog config
-export const DD_APP_ID = process.env.DD_APP_ID!;
-export const DD_CLIENT_TOKEN = process.env.DD_CLIENT_TOKEN!;
-export const DD_SITE = 'datadoghq.com';
-export const DD_SERVICE = process.env.NEXT_PUBLIC_DD_SERVICE!;
-export const ENABLE_DD = process.env.NEXT_PUBLIC_ENABLE_DD! === 'true';
-
-// AA tracking config
-export const AA_BASE_URL =
-  'https://autonomous-analytics-qffztaoryq-uc.a.run.app/api/v1';
-export const AA_CLIENT_TOKEN = process.env.AA_CLIENT_TOKEN!;
-export const AA_PLATFORM = 'web';
-
-// RAPI config
-export const RAPI_URL = 'https://telize-v1.p.rapidapi.com/geoip';
-export const RAPI_HOST = 'telize-v1.p.rapidapi.com';
-export const RAPID_CLIENT_TOKEN = process.env.RAPID_CLIENT_TOKEN!;
 export const SERVICE_FEE = 2.5 / 100;
 
 // Discount
