8f91c74d881a406274293d00ab591960002d169a
Dr. Cat <dr.cat911@generative.xyz>
2023-02-16 18:12:24 +0700

    fix: update name header, review thumbnail

--- src/containers/GenerativeProjectEdit/index.tsx
+++ src/containers/GenerativeProjectEdit/index.tsx
@@ -1,22 +1,14 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import {
   GenerativeProjectDetailContext,
   GenerativeProjectDetailProvider,
 } from '@contexts/generative-project-detail-context';
-import useWindowSize from '@hooks/useWindowSize';
-import { Token } from '@interfaces/token';
-import { base64ToUtf8, escapeSpecialChars } from '@utils/format';
-import ThumbnailPreview from '@components/ThumbnailPreview';
 import FormEditProject from '@containers/GenerativeProjectEdit/FormEditProject';
 import s from './styles.module.scss';
 import { Container } from 'react-bootstrap';
 
 const GenerativeProjectEdit = (): JSX.Element => {
-  const { mobileScreen } = useWindowSize();
-  const [projectDetail, setProjectDetail] = useState<Omit<Token, 'owner'>>();
-  const { projectData: project, isBitcoinProject } = useContext(
-    GenerativeProjectDetailContext
-  );
+  const { projectData } = useContext(GenerativeProjectDetailContext);
 
   const renderLeftContent = () => {
     return (
@@ -26,31 +18,13 @@ const GenerativeProjectEdit = (): JSX.Element => {
     );
   };
 
-  useEffect(() => {
-    if (!project) return;
-    const _projectDetail = base64ToUtf8(
-      project.projectURI.replace('data:application/json;base64,', '')
-    );
-    if (_projectDetail) {
-      let projectDetailJSON = _projectDetail;
-      if (!isBitcoinProject) {
-        projectDetailJSON = escapeSpecialChars(_projectDetail);
-      }
-      const projectDetailObj = JSON.parse(projectDetailJSON);
-      setProjectDetail(projectDetailObj);
-    }
-  }, [project?.id]);
-
   return (
     <Container>
       <div className={s.wrapper}>
         {renderLeftContent()}
-        <div />
-        {!mobileScreen && (
-          <div>
-            <ThumbnailPreview data={projectDetail as Token} allowVariantion />
-          </div>
-        )}
+        <div className={s.wrapper_image}>
+          <img src={projectData?.image} alt="projectData_image" />
+        </div>
       </div>
     </Container>
   );
