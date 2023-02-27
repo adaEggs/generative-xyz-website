89cf122411a44acde5585506d46ba2ab0a696d8f
hugrid9 <hugrid27@generative.xyz>
2023-02-27 21:39:25 +0700

    feat: buy with eth

--- src/interfaces/api/marketplace-btc.ts
+++ src/interfaces/api/marketplace-btc.ts
@@ -78,6 +78,7 @@ export interface IGetMarketplaceBtcNFTDetail {
 export interface ISubmitBTCAddressResponse {
   receiveAddress: string;
   timeoutAt: string;
+  price?: string;
 }
 
 export interface ISubmitBTCAddressPayload {
