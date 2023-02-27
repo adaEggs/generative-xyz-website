bad33af6dfa60e70eeadfce0578a5da50f9c9ad7
benjiro2077 <benjiro2077@generative.xyz>
2023-02-27 13:55:49 +0700

    fix: open corp generative preview

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ module.exports = withRemoveImports(
           headers: [
             {
               key: 'Content-Security-Policy',
-              value: `frame-ancestors 'self' https://ordinals.com https://dev-v5.generativeexplorer.com https://ordinals-explorer.generative.xyz https://cdn.generative.xyz https://preview.generativeexplorer.com; frame-src 'self' https://ordinals.com https://dev-v5.generativeexplorer.com https://ordinals-explorer.generative.xyz/ https://cdn.generative.xyz https://preview.generativeexplorer.com;`,
+              value: `frame-ancestors 'self' http://localhost:3000 https://generative.xyz https://ordinals.com https://dev-v5.generativeexplorer.com https://ordinals-explorer.generative.xyz https://cdn.generative.xyz https://preview.generativeexplorer.com https://storage.googleapis.com; frame-src 'self' http://localhost:3000 https://generative.xyz https://ordinals.com https://dev-v5.generativeexplorer.com https://ordinals-explorer.generative.xyz/ https://cdn.generative.xyz https://preview.generativeexplorer.com https://storage.googleapis.com;`,
             },
             ...baseSecurityHeaders,
           ],
