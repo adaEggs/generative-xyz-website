eb6507811b8ac5890929c7065791f6d78e6732c4
hugrid9 <hugrid27@generative.xyz>
2023-03-03 12:20:50 +0700

    feat: developer tab

--- src/containers/EditProfile/FormEditProfile/DeveloperTab/index.tsx
+++ src/containers/EditProfile/FormEditProfile/DeveloperTab/index.tsx
@@ -4,13 +4,11 @@ import SvgInset from '@components/SvgInset';
 import { CDN_URL } from '@constants/config';
 import { IApikey } from '@interfaces/api/profile';
 import copy from 'copy-to-clipboard';
-import React, { useCallback, useState } from 'react';
-import {
-  GoogleReCaptcha,
-  GoogleReCaptchaProvider,
-} from 'react-google-recaptcha-v3';
+import React, { useCallback, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import s from './styles.module.scss';
+
+const siteKey = '6LfM-cckAAAAALcEcvQGz1klpPruM-gvrBJRir1l';
 
 const DeveloperTab = ({
   loading,
@@ -39,46 +37,62 @@ const DeveloperTab = ({
     }
   };
 
+  const handleLoaded = () => {
+    window.grecaptcha.ready(() => {
+      window.grecaptcha
+        .execute(siteKey, { action: 'homepage' })
+        .then((token: string) => {
+          onVerify(token);
+        });
+    });
+  };
+
+  useEffect(() => {
+    // Add reCaptcha
+    const script = document.createElement('script');
+    script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}`;
+    script.addEventListener('load', handleLoaded);
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className={s.container}>
-      <GoogleReCaptchaProvider reCaptchaKey="6LfM-cckAAAAALcEcvQGz1klpPruM-gvrBJRir1l">
-        {!loading && (
-          <div>
-            {apiKey ? (
-              <div>
-                <p className={s.titleKey}>YOUR API KEY</p>
-                <div className={s.apiKey}>
-                  <p>{apiKey.apiKey}</p>
-                  <SvgInset
-                    className={s.icCopy}
-                    size={18}
-                    svgUrl={`${CDN_URL}/icons/ic-copy.svg`}
-                    onClick={() => onClickCopy(apiKey.apiKey)}
-                  />
-                </div>
+      {!loading && (
+        <div>
+          {apiKey ? (
+            <div>
+              <p className={s.titleKey}>YOUR API KEY</p>
+              <div className={s.apiKey}>
+                <p>{apiKey.apiKey}</p>
+                <SvgInset
+                  className={s.icCopy}
+                  size={18}
+                  svgUrl={`${CDN_URL}/icons/ic-copy.svg`}
+                  onClick={() => onClickCopy(apiKey.apiKey)}
+                />
               </div>
-            ) : (
-              <div>
-                <div>
-                  <GoogleReCaptcha onVerify={onVerify} />
-                </div>
-                <ButtonIcon
-                  onClick={onClickGenerate}
-                  className={s.submit_btn}
-                  disabled={!token}
-                >
-                  Generate Api key
-                </ButtonIcon>
-              </div>
-            )}
-          </div>
-        )}
-        {loading && (
-          <div className={s.loadingWrapper}>
-            <Loading isLoaded={false} />
-          </div>
-        )}
-      </GoogleReCaptchaProvider>
+            </div>
+          ) : (
+            <div>
+              <ButtonIcon
+                onClick={onClickGenerate}
+                className={s.submit_btn}
+                disabled={!token}
+              >
+                Generate api key
+              </ButtonIcon>
+            </div>
+          )}
+        </div>
+      )}
+      {loading && (
+        <div className={s.loadingWrapper}>
+          <Loading isLoaded={false} />
+        </div>
+      )}
     </div>
   );
 };
