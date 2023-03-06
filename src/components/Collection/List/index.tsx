0224da6e16df5d7afe8709645cb8b13c2feb3f83
Archaon <archaon@generative.xyz>
2023-03-06 14:50:16 +0700

    feat: update filter ui ux

--- src/components/Collection/List/index.tsx
+++ src/components/Collection/List/index.tsx
@@ -8,6 +8,11 @@ import FilterOptions from '../FilterOptions';
 import CollectionListLoading from '../Loading';
 import s from './CollectionList.module.scss';
 import { GenerativeProjectDetailContext } from '@contexts/generative-project-detail-context';
+import ButtonIcon from '@components/ButtonIcon';
+import SvgInset from '@components/SvgInset';
+import { CDN_URL } from '@constants/config';
+import { v4 } from 'uuid';
+import Text from '@components/Text';
 
 const CollectionList = ({
   listData,
@@ -18,7 +23,9 @@ const CollectionList = ({
   projectInfo?: Project | null;
   isLoaded?: boolean;
 }) => {
-  const { showFilter } = useContext(GenerativeProjectDetailContext);
+  const { showFilter, filterTraits, setFilterTraits } = useContext(
+    GenerativeProjectDetailContext
+  );
 
   const hasTraitAtrribute = useMemo(
     () => projectInfo?.traitStat && projectInfo?.traitStat?.length > 0,
@@ -35,35 +42,68 @@ const CollectionList = ({
       {showFilter && hasTraitAtrribute && (
         <FilterOptions attributes={projectInfo?.traitStat} />
       )}
-      <div className="position-relative">
-        <div className="filter-list"></div>
-        {!isLoaded && (
-          <>
-            <CollectionListLoading
-              numOfItems={12}
-              showFilter={hasTraitAtrribute}
-            />
-          </>
-        )}
-
-        {isLoaded && (
-          <div className={cs(s.collectionList, `row animate-grid`)}>
-            {listData?.map(item => (
-              <CollectionItem
-                className={`${
-                  hasTraitAtrribute
-                    ? 'col-wide-3 col-xl-4 col-12'
-                    : 'col-wide-2_5 col-xl-3 col-lg-4 col-12'
-                } `}
-                key={`collection-item-${item.tokenID}`}
-                data={item}
-              />
+      <div>
+        {filterTraits && filterTraits.length > 0 && (
+          <div className={s.filterList}>
+            {filterTraits.split(',').map(trait => (
+              <div
+                key={`trait-${v4()}`}
+                className={cs(s.filterItem, 'd-flex align-items-center')}
+              >
+                <Text>{`${trait.split(':')[0]}: ${trait.split(':')[1]}`}</Text>
+                <SvgInset
+                  size={8}
+                  svgUrl={`${CDN_URL}/icons/ic-close.svg`}
+                  className={s.removeIcon}
+                />
+              </div>
             ))}
+            <ButtonIcon onClick={() => setFilterTraits('')}>
+              Clear all
+            </ButtonIcon>
           </div>
         )}
-        {isLoaded && listData && listData?.length === 0 && (
-          <Empty projectInfo={projectInfo} className={s.list_empty} />
-        )}
+
+        <div className="position-relative">
+          {!isLoaded && (
+            <>
+              <CollectionListLoading
+                numOfItems={12}
+                showFilter={hasTraitAtrribute}
+              />
+            </>
+          )}
+
+          {isLoaded && (
+            <div className={cs(s.collectionList, `row animate-grid`)}>
+              {listData?.map(item => (
+                <CollectionItem
+                  className={`${
+                    hasTraitAtrribute
+                      ? 'col-wide-3 col-xl-4 col-12'
+                      : 'col-wide-2_5 col-xl-3 col-lg-4 col-12'
+                  } `}
+                  key={`collection-item-${item.tokenID}`}
+                  data={item}
+                />
+              ))}
+            </div>
+          )}
+          {isLoaded && listData && listData?.length === 0 && (
+            <Empty
+              projectInfo={projectInfo}
+              className={s.list_empty}
+              content={
+                !!filterTraits && (
+                  <>
+                    <p>No available results for selected filters.</p>
+                    <p>Please try again.</p>
+                  </>
+                )
+              }
+            />
+          )}
+        </div>
       </div>
     </div>
   );
