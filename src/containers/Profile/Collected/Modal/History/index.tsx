472181cc11c08546635c079b2d561f3e6d390989
SangNX <matterhorn@generative.xyz>
2023-03-07 16:24:50 +0700

    fix: show history type

--- src/containers/Profile/Collected/Modal/History/index.tsx
+++ src/containers/Profile/Collected/Modal/History/index.tsx
@@ -10,6 +10,7 @@ import Table from '@components/Table';
 import Text from '@components/Text';
 import { formatUnixDateTime } from '@utils/time';
 import { Stack } from 'react-bootstrap';
+import { TrackTxType } from '@interfaces/api/bitcoin';
 
 interface IProps {
   showModal: boolean;
@@ -29,67 +30,54 @@ const HistoryModal = ({ showModal, onClose }: IProps): JSX.Element => {
     toast.success('Copied');
   };
 
-  const tableData = (history || []).map(item => {
-    return {
-      id: `${item.txhash}-history`,
-      render: {
-        date: (
-          <Text size="16" fontWeight="medium" color="black-100">
-            {item.created_at
-              ? formatUnixDateTime({ dateTime: Number(item.created_at) })
-              : '---'}
-          </Text>
-        ),
-        hash: (
-          <>
-            <div style={{ width: 'fit-content' }}>
-              <Stack direction="horizontal" gap={3}>
-                <Text size="16" fontWeight="medium" color="black-100">
-                  {ellipsisCenter({ str: item.txhash, limit: 6 })}
-                </Text>
-                <SvgInset
-                  size={18}
-                  svgUrl={`${CDN_URL}/icons/ic-copy.svg`}
-                  className={s.wrapHistory_copy}
-                  onClick={() => handleCopy(item.txhash)}
-                />
-                <SvgInset
-                  size={16}
-                  svgUrl={`${CDN_URL}/icons/ic-share.svg`}
-                  className={s.wrapHistory_copy}
-                  onClick={() =>
-                    window.open(
-                      `https://www.blockchain.com/explorer/transactions/btc/${item.txhash}`
-                    )
-                  }
-                />
-              </Stack>
-              <Text
-                size="16"
-                fontWeight="medium"
-                style={{ color: item.statusColor }}
-              >
-                {item.status}
-              </Text>
-            </div>
-          </>
-        ),
-        number: (
-          <>
-            {!!item.inscription_id && !!item.inscription_number ? (
-              <div>
+  const mapTxType = (type: TrackTxType) => {
+    switch (type) {
+      case TrackTxType.normal:
+        return 'Send BTC';
+      case TrackTxType.inscription:
+        return 'Send inscription';
+      case TrackTxType.list:
+        return 'List for sale';
+      case TrackTxType.buyInscription:
+        return 'Buy inscription';
+      // case TrackTxType.buySplit:
+      //   return 'Buy split';
+      // case TrackTxType.listSplit:
+      //   return 'List split';
+      case TrackTxType.cancel:
+        return 'Cancel listing';
+    }
+  };
+
+  const tableData = (history || [])
+    .filter(
+      item =>
+        item.type !== TrackTxType.buySplit &&
+        item.type !== TrackTxType.listSplit
+    )
+    .map(item => {
+      return {
+        id: `${item.txhash}-history`,
+        render: {
+          date: (
+            <Text size="16" fontWeight="medium" color="black-100">
+              {item.created_at
+                ? formatUnixDateTime({ dateTime: Number(item.created_at) })
+                : '---'}
+            </Text>
+          ),
+          hash: (
+            <>
+              <div style={{ width: 'fit-content' }}>
                 <Stack direction="horizontal" gap={3}>
                   <Text size="16" fontWeight="medium" color="black-100">
-                    {`${ellipsisCenter({
-                      str: item.inscription_id,
-                      limit: 6,
-                    })}`}
+                    {ellipsisCenter({ str: item.txhash, limit: 6 })}
                   </Text>
                   <SvgInset
                     size={18}
                     svgUrl={`${CDN_URL}/icons/ic-copy.svg`}
                     className={s.wrapHistory_copy}
-                    onClick={() => handleCopy(item.inscription_id)}
+                    onClick={() => handleCopy(item.txhash)}
                   />
                   <SvgInset
                     size={16}
@@ -97,32 +85,75 @@ const HistoryModal = ({ showModal, onClose }: IProps): JSX.Element => {
                     className={s.wrapHistory_copy}
                     onClick={() =>
                       window.open(
-                        `https://ordinals.com/inscription/${item.inscription_id}`
+                        `https://www.blockchain.com/explorer/transactions/btc/${item.txhash}`
                       )
                     }
                   />
                 </Stack>
-                <Text size="16" fontWeight="medium" color="black-100">
-                  {`#${item.inscription_number}`}
+                <Text
+                  size="16"
+                  fontWeight="medium"
+                  style={{ color: item.statusColor }}
+                >
+                  {item.status}
                 </Text>
               </div>
-            ) : (
+            </>
+          ),
+          number: (
+            <>
+              {!!item.inscription_id && !!item.inscription_number ? (
+                <div>
+                  <Stack direction="horizontal" gap={3}>
+                    <Text size="16" fontWeight="medium" color="black-100">
+                      {`${ellipsisCenter({
+                        str: item.inscription_id,
+                        limit: 6,
+                      })}`}
+                    </Text>
+                    <SvgInset
+                      size={18}
+                      svgUrl={`${CDN_URL}/icons/ic-copy.svg`}
+                      className={s.wrapHistory_copy}
+                      onClick={() => handleCopy(item.inscription_id)}
+                    />
+                    <SvgInset
+                      size={16}
+                      svgUrl={`${CDN_URL}/icons/ic-share.svg`}
+                      className={s.wrapHistory_copy}
+                      onClick={() =>
+                        window.open(
+                          `https://ordinals.com/inscription/${item.inscription_id}`
+                        )
+                      }
+                    />
+                  </Stack>
+                  <Text size="16" fontWeight="medium" color="black-100">
+                    {`#${item.inscription_number}`}
+                  </Text>
+                </div>
+              ) : (
+                <Text size="16" fontWeight="medium" color="black-100">
+                  ---
+                </Text>
+              )}
+            </>
+          ),
+          amount: (
+            <>
               <Text size="16" fontWeight="medium" color="black-100">
-                ---
+                {item.send_amount
+                  ? `${formatBTCPrice(item.send_amount.toString())} BTC`
+                  : '---'}
               </Text>
-            )}
-          </>
-        ),
-        amount: (
-          <Text size="16" fontWeight="medium" color="black-100">
-            {item.send_amount
-              ? `${formatBTCPrice(item.send_amount.toString())} BTC`
-              : '---'}
-          </Text>
-        ),
-      },
-    };
-  });
+              <Text size="16" fontWeight="medium" color="black-100">
+                {mapTxType(item.type)}
+              </Text>
+            </>
+          ),
+        },
+      };
+    });
 
   if (!showModal) {
     return <></>;
