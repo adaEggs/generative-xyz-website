07889819f15420616b5da9e00fef95c985c4f81d
Minh Tam <tam@autonomous.nyc>
2023-03-09 13:47:53 +0700

    feat: update  inscription page

--- src/pages/live/[tokenID]/index.tsx
+++ src/pages/live/[tokenID]/index.tsx
@@ -1,22 +1,40 @@
 import { GetServerSidePropsContext, NextPage } from 'next';
+import { SWRConfig } from 'swr';
+
 import MarketplaceLayout from '@layouts/Marketplace';
 import InscriptionID from '@containers/Trade/InscriptionID';
 import { CDN_URL } from '@constants/config';
 import { SEO_TITLE } from '@constants/seo-default-info';
 import { getOrdinalImgURL } from '@utils/inscribe';
 import { getInscriptionDetail } from '@services/marketplace-btc';
+import { getApiKey } from '@utils/swr';
 
-const InscriptionIDPage: NextPage = () => {
+interface IPropsPage {
+  fallback: Record<string, string> | null;
+}
+
+const InscriptionIDPage: NextPage<IPropsPage> = ({ fallback }) => {
   return (
     <MarketplaceLayout>
-      <InscriptionID />
+      <SWRConfig
+        value={{
+          revalidateOnFocus: true,
+          revalidateIfStale: false,
+          fetcher: getInscriptionDetail,
+          fallback: fallback || {},
+        }}
+      >
+        <InscriptionID />
+      </SWRConfig>
     </MarketplaceLayout>
   );
 };
 
 export default InscriptionIDPage;
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   try {
     const { query } = context;
     const { tokenID } = query as { tokenID: string };
@@ -31,6 +49,9 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
           image:
             getOrdinalImgURL(tokenID) || `${CDN_URL}/images/collection.jpg`,
         },
+        fallback: {
+          [getApiKey(getInscriptionDetail, tokenID)]: tokenData,
+        },
       },
     };
   } catch (err) {
@@ -41,7 +62,8 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
           description: 'Bitcoin NFTs. Browse. Curate. Purchase.',
           image: `${CDN_URL}/images/collection.jpg`,
         },
+        fallback: {},
       },
     };
   }
-}
+};
